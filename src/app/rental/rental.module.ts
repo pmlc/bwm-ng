1dc0963251b89147e5fad5d0ebaf885f0165d841
Pierre Le Count <pmlc@iinet.net.au>
2018-11-24 03:33:00 +0000

    Rentals - more work on input data

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -5,6 +5,8 @@ import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalComponent } from './rental.component';
 
+import { RentalService } from './shared/rental.service';
+
 
 @NgModule({ 
    declarations: [
@@ -12,7 +14,8 @@ import { RentalComponent } from './rental.component';
       RentalListItemComponent,
       RentalComponent
    ],
-   imports: [CommonModule]
+   imports: [CommonModule],
+   providers: [RentalService] 
 })
 
 

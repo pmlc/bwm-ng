62a9b57faf1fd6cbe240557980d86ebfddcee5f0
Pierre Le Count <pmlc@iinet.net.au>
2018-11-25 02:02:51 +0000

    Rentals - more work with input - ch45

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
 
 @Component({
   selector: 'bwm-rental-list',
@@ -8,17 +9,17 @@ import { RentalService } from '../shared/rental.service';
 })
 export class RentalListComponent implements OnInit {
 
-  rentals: any[] = []
+  rentals: Rental[] = [];
   
   constructor(private rentalService: RentalService) { }
   
   constructor() { }
 
   ngOnInit() {
-    const rentalObservable = this.rentalService.getRentals();
+    const rentalObservable = this.rentalService.getRentals()
     
     rentalObservable.subscribe(
-      (rentals) => {
+      (rentals: Rental[]) => {
         this.rentals = rentals;
       };
       (err) => {
